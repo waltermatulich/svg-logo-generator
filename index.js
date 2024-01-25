2e14fc3fb0b3a4e84f9f56569f0f3d2c3cbecfd1
waltermatulich <wmatulich231@gmail.com>
2024-01-24 22:14:48 -0800

    Added changes to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+function createLogo() {
+    // ... (use the classes from the lib folder)
+}
+
+function createShapeElement(shape, color) {
+    // ... (use the classes from the lib folder)
+}
+
+module.exports = { createLogo };
